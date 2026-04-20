ee060b914a84e6774c08f6089bdb360a6f3962fb
Georgii Oblapenko <kunstmord@kunstmord.com>
2026-04-20 21:04:27 +0200

    placeholder

---
+++ test/test_find_maxwellian.jl
@@ -0,0 +1,4 @@
+@testset "find Maxwellian approximation" begin
+    # find_MB_solution!(vdf_mb, grid, ref_moms_constraint, vx_index, vy_index, vz_index,
+    #                        Ex_index, Ey_index, Ez_index; tol=1e-11)
+end
